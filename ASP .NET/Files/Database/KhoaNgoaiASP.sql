079ae7684eda34f9d0e63f24cba6bea2f94a356d
NhatTaiC <nhattai0501@gmail.com>
2024-10-11 10:49:37 +0700

    Lap Trinh ASP .NET - TDC - 2024 Tuan: 4 Ngay: 11/10/2024 De tai: Dang ky tham quan doanh nghiep cua sinh vien TDC Nhom: 5 Chau Nhat Tai Nguyen Quoc Luong Le Van Toan Nguyen Phong Phu Pham The Minh
    Noi dung cong viec:
    - [x] Changed Files -> files
    - [x] Added folder Report
    - [x] Removed KhoaNgoaiASP.sql in Folder Database
    - [x] Added DKTQDN_script.sql in Folder Database
    - [x] Added Nhom 5 - DKTQDN.docx
    - [x] Nhom 5 - DKTQDN.pdf
    
    /* Committed? */
    - [x] Yes/No
    
    /* Note */
    - [x] Changed DB name DangKyThamQuanDoanhNghiep -> DKTQDN
    - [x] Uploaded file again

--- ASP .NET/Files/Database/KhoaNgoaiASP.sql
+++
@@ -1,7 +0,0 @@
-DECLARE @sql NVARCHAR(MAX) = '';
-
-SELECT @sql += 'ALTER TABLE ' + QUOTENAME(OBJECT_SCHEMA_NAME(parent_object_id)) + '.' + QUOTENAME(OBJECT_NAME(parent_object_id)) + 
-               ' DROP CONSTRAINT ' + QUOTENAME(name) + ';' + CHAR(13)
-FROM sys.foreign_keys;
-
-EXEC sp_executesql @sql;
