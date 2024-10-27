d69c32b70803a5efc08220fdc4e37ebd3a5fc5cc
LVanToan <toanl7288@gmail.com>
2024-10-27 20:27:44 +0700

    update source code

--- ASP .NET/Files/src code/Nhom5_ASP_DKTQDN/Nhom5_ASP_DKTQDN/Program.cs
+++ ASP .NET/Files/src code/Nhom5_ASP_DKTQDN/Nhom5_ASP_DKTQDN/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Nhom5_ASP_DKTQDN.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<DKTQDNContext>(option=>option.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
