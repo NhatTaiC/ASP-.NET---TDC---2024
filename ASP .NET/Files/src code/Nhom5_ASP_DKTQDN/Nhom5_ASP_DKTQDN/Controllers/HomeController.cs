f37e4ab94a8d801f5efe550f4d08f05deb158718
Luong2712 <nguyenquocluong2712@gmail.com>
2024-10-27 18:11:49 +0700

    task2-Luong

--- ASP .NET/Files/src code/Nhom5_ASP_DKTQDN/Nhom5_ASP_DKTQDN/Controllers/HomeController.cs
+++ ASP .NET/Files/src code/Nhom5_ASP_DKTQDN/Nhom5_ASP_DKTQDN/Controllers/HomeController.cs
@@ -7,10 +7,11 @@ namespace Nhom5_ASP_DKTQDN.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DKTQDNContext _dKTQDNContext;
+        public HomeController(ILogger<HomeController> logger, DKTQDNContext dKTQDN)
         {
             _logger = logger;
+            _dKTQDNContext = dKTQDN;
         }
 
         public IActionResult Index()
@@ -20,13 +21,43 @@ namespace Nhom5_ASP_DKTQDN.Controllers
 
         public IActionResult Privacy()
         {
+            var list = _dKTQDNContext.TaiKhoans.ToList();
             return View();
         }
-
+        public IActionResult TaiKhoanList()
+        {
+            var tklist = _dKTQDNContext.TaiKhoans.ToList();
+            return View(tklist);
+        }
+        public IActionResult CreateTaiKhoan()
+        {
+            //var tklist = _dKTQDNContext.TaiKhoans.ToList();
+            return View();
+        }
+        public IActionResult CreateKhoaHoc()
+        {
+            //var tklist = _dKTQDNContext.TaiKhoans.ToList();
+            return View();
+        }
+        public IActionResult EditKhoaHoc()
+        {
+            //var tklist = _dKTQDNContext.TaiKhoans.ToList();
+            return View();
+        }
+        public IActionResult EditTaiKhoan(int id)
+        {
+            var tklist = _dKTQDNContext.TaiKhoans.FirstOrDefault(tklist => tklist.Id == id);
+            return View(tklist);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult KhoaHocList()
+        {
+            var khohocs = _dKTQDNContext.KhoaHocs.ToList();
+            return View(khohocs);
         }
     }
 }
---
+++ ASP .NET/Files/src code/Nhom5_ASP_DKTQDN/Nhom5_ASP_DKTQDN/Models/DKTQDNContext.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Nhom5_ASP_DKTQDN.Models
+{
+    public partial class DKTQDNContext : DbContext
+    {
+        public DKTQDNContext()
+        {
+        }
+
+        public DKTQDNContext(DbContextOptions<DKTQDNContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ChiTietDangKy> ChiTietDangKies { get; set; } = null!;
+        public virtual DbSet<ChuyenDi> ChuyenDis { get; set; } = null!;
+        public virtual DbSet<DoanhNghiep> DoanhNghieps { get; set; } = null!;
+        public virtual DbSet<GiangVien> GiangViens { get; set; } = null!;
+        public virtual DbSet<Khoa> Khoas { get; set; } = null!;
+        public virtual DbSet<KhoaHoc> KhoaHocs { get; set; } = null!;
+        public virtual DbSet<SinhVien> SinhViens { get; set; } = null!;
+        public virtual DbSet<TaiKhoan> TaiKhoans { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=.\\sqlexpress; Database=DKTQDN; Trusted_Connection =True;");
+//            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChiTietDangKy>(entity =>
+            {
+                entity.ToTable("ChiTietDangKy");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.CreatedBy).HasColumnName("created_by");
+
+                entity.Property(e => e.IdChuyenDi).HasColumnName("idChuyenDi");
+
+                entity.Property(e => e.IdSinhVien).HasColumnName("idSinhVien");
+
+                entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(e => e.NgayDangKy).HasColumnType("date");
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("updated_at");
+
+                entity.Property(e => e.UpdatedBy).HasColumnName("updated_by");
+
+                entity.HasOne(d => d.IdChuyenDiNavigation)
+                    .WithMany(p => p.ChiTietDangKies)
+                    .HasForeignKey(d => d.IdChuyenDi)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ChiTietDangKy_ChuyenDi");
+
+                entity.HasOne(d => d.IdSinhVienNavigation)
+                    .WithMany(p => p.ChiTietDangKies)
+                    .HasForeignKey(d => d.IdSinhVien)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ChiTietDangKy_SinhVien");
+            });
+
+            modelBuilder.Entity<ChuyenDi>(entity =>
+            {
+                entity.ToTable("ChuyenDi");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.CreatedBy).HasColumnName("created_by");
+
+                entity.Property(e => e.IdDoanhNghiep).HasColumnName("idDoanhNghiep");
+
+                entity.Property(e => e.IdGiangVien).HasColumnName("idGiangVien");
+
+                entity.Property(e => e.IdKhoa).HasColumnName("idKhoa");
+
+                entity.Property(e => e.IdKhoaHoc).HasColumnName("idKhoaHoc");
+
+                entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(e => e.MaChuyenDi)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NamHoc).HasColumnType("date");
+
+                entity.Property(e => e.NgayThamQuan).HasColumnType("date");
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("updated_at");
+
+                entity.Property(e => e.UpdatedBy).HasColumnName("updated_by");
+
+                entity.HasOne(d => d.IdDoanhNghiepNavigation)
+                    .WithMany(p => p.ChuyenDis)
+                    .HasForeignKey(d => d.IdDoanhNghiep)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ChuyenDi_DoanhNghiep");
+
+                entity.HasOne(d => d.IdGiangVienNavigation)
+                    .WithMany(p => p.ChuyenDis)
+                    .HasForeignKey(d => d.IdGiangVien)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ChuyenDi_GiangVien");
+
+                entity.HasOne(d => d.IdKhoaNavigation)
+                    .WithMany(p => p.ChuyenDis)
+                    .HasForeignKey(d => d.IdKhoa)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ChuyenDi_Khoa");
+
+                entity.HasOne(d => d.IdKhoaHocNavigation)
+                    .WithMany(p => p.ChuyenDis)
+                    .HasForeignKey(d => d.IdKhoaHoc)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ChuyenDi_KhoaHoc");
+            });
+
+            modelBuilder.Entity<DoanhNghiep>(entity =>
+            {
+                entity.ToTable("DoanhNghiep");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.CreatedBy).HasColumnName("created_by");
+
+                entity.Property(e => e.DiaChi).HasMaxLength(100);
+
+                entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(e => e.MaDoanhNghiep)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SoDienThoai)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TenDoanhNghiep).HasMaxLength(100);
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("updated_at");
+
+                entity.Property(e => e.UpdatedBy).HasColumnName("updated_by");
+            });
+
+            modelBuilder.Entity<GiangVien>(entity =>
+            {
+                entity.ToTable("GiangVien");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.CreatedBy).HasColumnName("created_by");
+
+                entity.Property(e => e.IdKhoa).HasColumnName("idKhoa");
+
+                entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(e => e.MaGiangVien)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SoDienThoai)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TenGiangVien).HasMaxLength(100);
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("updated_at");
+
+                entity.Property(e => e.UpdatedBy).HasColumnName("updated_by");
+
+                entity.HasOne(d => d.IdKhoaNavigation)
+                    .WithMany(p => p.GiangViens)
+                    .HasForeignKey(d => d.IdKhoa)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_GiangVien_Khoa");
+            });
+
+            modelBuilder.Entity<Khoa>(entity =>
+            {
+                entity.ToTable("Khoa");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.CreatedBy).HasColumnName("created_by");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(e => e.MaKhoa)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SoDienThoai)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TenKhoa)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("updated_at");
+
+                entity.Property(e => e.UpdatedBy).HasColumnName("updated_by");
+            });
+
+            modelBuilder.Entity<KhoaHoc>(entity =>
+            {
+                entity.ToTable("KhoaHoc");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.CreatedBy).HasColumnName("created_by");
+
+                entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(e => e.MaKhoaHoc)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("updated_at");
+
+                entity.Property(e => e.UpdatedBy).HasColumnName("updated_by");
+            });
+
+            modelBuilder.Entity<SinhVien>(entity =>
+            {
+                entity.ToTable("SinhVien");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.CreatedBy).HasColumnName("created_by");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdKhoa).HasColumnName("idKhoa");
+
+                entity.Property(e => e.IdKhoaHoc).HasColumnName("idKhoaHoc");
+
+                entity.Property(e => e.IdTaiKhoan).HasColumnName("idTaiKhoan");
+
+                entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(e => e.MaSinhVien)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SoDienThoai)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TenSinhVien).HasMaxLength(100);
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("updated_at");
+
+                entity.Property(e => e.UpdatedBy).HasColumnName("updated_by");
+
+                entity.HasOne(d => d.IdKhoaNavigation)
+                    .WithMany(p => p.SinhViens)
+                    .HasForeignKey(d => d.IdKhoa)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_SinhVien_Khoa");
+
+                entity.HasOne(d => d.IdKhoaHocNavigation)
+                    .WithMany(p => p.SinhViens)
+                    .HasForeignKey(d => d.IdKhoaHoc)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_SinhVien_KhoaHoc");
+
+                entity.HasOne(d => d.IdTaiKhoanNavigation)
+                    .WithMany(p => p.SinhViens)
+                    .HasForeignKey(d => d.IdTaiKhoan)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_SinhVien_TaiKhoan");
+            });
+
+            modelBuilder.Entity<TaiKhoan>(entity =>
+            {
+                entity.ToTable("TaiKhoan");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.CreatedBy).HasColumnName("created_by");
+
+                entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(e => e.MatKhau)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TenTaiKhoan)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("updated_at");
+
+                entity.Property(e => e.UpdatedBy).HasColumnName("updated_by");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
