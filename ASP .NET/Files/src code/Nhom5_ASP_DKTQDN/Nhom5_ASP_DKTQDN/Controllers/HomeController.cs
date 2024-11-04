ed7c596978ed8a20d0448c47cce40eaa4463ed9b
22211tt0826 <Phuynguyen9484@gmail.com>
2024-11-04 20:05:44 +0700

    cap nhat lan 2

--- ASP .NET/Files/src code/Nhom5_ASP_DKTQDN/Nhom5_ASP_DKTQDN/Controllers/HomeController.cs
+++ ASP .NET/Files/src code/Nhom5_ASP_DKTQDN/Nhom5_ASP_DKTQDN/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using Nhom5_ASP_DKTQDN.Models;
 using System.Diagnostics;
+using X.PagedList;
 
 namespace Nhom5_ASP_DKTQDN.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DKTQDNContext _dKTQDNContext;
+        public HomeController(ILogger<HomeController> logger, DKTQDNContext dKTQDN)
         {
             _logger = logger;
+            _dKTQDNContext = dKTQDN;
         }
 
         public IActionResult Index()
@@ -20,13 +23,96 @@ namespace Nhom5_ASP_DKTQDN.Controllers
 
         public IActionResult Privacy()
         {
+            var list = _dKTQDNContext.TaiKhoans.ToList();
             return View();
         }
+        public IActionResult TaiKhoanList()
+        {
+            var tklist = _dKTQDNContext.TaiKhoans.ToList();
+            return View(tklist);
+        }
+        public IActionResult CreateTaiKhoan()
+        {
+            //var tklist = _dKTQDNContext.TaiKhoans.ToList();
+            return View();
+        }
+        public IActionResult CreateKhoaHoc()
+        {
+            //var tklist = _dKTQDNContext.TaiKhoans.ToList();
+            return View();
+        }
+        public IActionResult EditKhoaHoc()
+        {
+            //var tklist = _dKTQDNContext.TaiKhoans.ToList();
+            return View();
+        }
+        public IActionResult KhoaList(int? page)
+        {
+            var khoaList = _dKTQDNContext.Khoas.ToPagedList(page ?? 1, 10);
+            return View(khoaList);
+        }
+        public IActionResult DoanhNghiepList(int? page)
+        {
+            var doanhNghiepList = _dKTQDNContext.DoanhNghieps.ToPagedList(page ?? 1, 10);
+            return View(doanhNghiepList);
+        }
+        public IActionResult CreateKhoa(Khoa obj)
+        {   
+            _dKTQDNContext.Add(obj);
+            _dKTQDNContext.SaveChanges();
+            return RedirectToAction("KhoaList");
+        }
+        public IActionResult CreateDoanhNghiep(DoanhNghiep obj)
+        {
+            _dKTQDNContext.Add(obj);
+            _dKTQDNContext.SaveChanges();
+            return RedirectToAction("DoanhNghiepList");
+        }
+        public IActionResult DeleteKhoa(int id)
+        {
+            Khoa obj = _dKTQDNContext.Khoas.SingleOrDefault(d => d.Id == id);
+            if (obj != null)
+            {
+                _dKTQDNContext.Khoas.Remove(obj);
+                _dKTQDNContext.SaveChanges();
+            }
+            return RedirectToAction("DoanhNghiepList");
+        }
+        public IActionResult DeleteDoanhNghiep(int id)
+        {
+            DoanhNghiep obj = _dKTQDNContext.DoanhNghieps.SingleOrDefault(d => d.Id == id);
+            if (obj != null)
+            {
+                _dKTQDNContext.DoanhNghieps.Remove(obj);
+                _dKTQDNContext.SaveChanges();
+            }
+            return RedirectToAction("KhoaList");
+        }
+        public IActionResult KhoaUpdate(int? id)
+        {
+            Khoa objKhoa = _dKTQDNContext.Khoas.FirstOrDefault(d => d.Id == id);
+            return View(objKhoa);
+        }
+        public IActionResult DoanhNghiepUpdate(int? id)
+        {
+            DoanhNghiep objDoanhNghiep = _dKTQDNContext.DoanhNghieps.FirstOrDefault(d => d.Id == id);
+            return View(objDoanhNghiep);
+        }
 
+        public IActionResult EditTaiKhoan(int id)
+        {
+            var tklist = _dKTQDNContext.TaiKhoans.FirstOrDefault(tklist => tklist.Id == id);
+            return View(tklist);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult KhoaHocList()
+        {
+            var khohocs = _dKTQDNContext.KhoaHocs.ToList();
+            return View(khohocs);
         }
     }
 }
